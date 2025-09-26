130394744f9afdd2149a4f9a5aab9a132c2b9e7e
namth1410 <tranhainam1410@gmail.com>
2025-09-26 17:17:26 +0700

    feat: update ui

--- src/components/loading-screen/LoadingScreen.tsx
+++ src/components/loading-screen/LoadingScreen.tsx
@@ -5,7 +5,7 @@ const LoadingScreen = () => {
   return (
     <div className={styles.loadingScreen}>
       <Spin size="large" />
-      <p className={styles.loadingText}>Loading khù khù khá...</p>
+      <p className={styles.loadingText}>Loading khù khù kh...</p>
     </div>
   );
 };
